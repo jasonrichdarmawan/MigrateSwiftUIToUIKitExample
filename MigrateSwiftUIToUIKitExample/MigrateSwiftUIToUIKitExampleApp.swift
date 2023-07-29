bbc39ca0e120e080d035a4227c9743e45f7d5ace
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-30 03:03:20 +0700

    Initial Commit

---
+++ MigrateSwiftUIToUIKitExample/MigrateSwiftUIToUIKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigrateSwiftUIToUIKitExampleApp.swift
+//  MigrateSwiftUIToUIKitExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MigrateSwiftUIToUIKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
